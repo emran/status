3d4c34ad0a4d4ec52801366266bfa229974c85bc
Emran ul Hadi <emran_cse0517@yahoo.com>
2013-01-06 21:41:18 +0600

    bootstrap changed

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -80,7 +80,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  caching     enable or disable internal caching                 FALSE
  */
 Kohana::init(array(
-    'base_url'   => '/status/',
+    'base_url'   => '/statusLoger/',
     'index_file' => ''
 ));
 
